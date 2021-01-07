64a721aca820cd2ac6c6e9ca7b98927720805a39
Roshith R <r.roshith@gmail.com>
2021-01-07 08:54:43 +0530

    added formatted date

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -14,12 +14,13 @@ import { v4 as uuidv4 } from 'uuid'
 import useFormStyles from './styles/index'
 import { ExpenseTrackerContext } from '../../context'
 import { incomeCategories, expenseCategories } from '../../constants'
+import { formatDate } from '../../utils/DateFormat'
 
 const initialState = {
   amount: '',
   category: '',
   type: 'Income',
-  date: new Date(),
+  date: formatDate(new Date()),
 }
 export const Form = () => {
   const classes = useFormStyles()
@@ -88,7 +89,7 @@ export const Form = () => {
           label='Date'
           fullWidth
           value={formData.date}
-          onChange={(e) => setFormData({ ...formData, date: e.target.value })}
+          onChange={(e) => setFormData({ ...formData, date: formatDate(e.target.value) })}
         />
       </Grid>
       <Button
